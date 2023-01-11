35399ec30f935c4ce24ccf5c0cd3cb5153dd0025
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-01-11 09:29:09 +0530

    Second

--- app/src/main/java/com/ashu/hyd/AuthenticationActivity.kt
+++ app/src/main/java/com/ashu/hyd/AuthenticationActivity.kt
@@ -1,5 +1,6 @@
 package com.ashu.hyd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,8 +9,9 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.auth.FirebaseAuth
 
-class AuthenticationActivity : AppCompatActivity() {
+class AuthenticationActivity : AppCompatActivity(),FirebaseAuth.AuthStateListener{
     private lateinit var viewPager : ViewPager2
     private lateinit var tabLayout : TabLayout
     private lateinit var viewPagerAdapter : AuthenticationPagerAdapter
@@ -41,5 +43,33 @@ class AuthenticationActivity : AppCompatActivity() {
                 else->Login()
             }
         }
+    }
+
+    override fun onAuthStateChanged(p0: FirebaseAuth) {
+          if(p0.currentUser!=null)
+          {
+              startMainActivity()
+          }
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this@AuthenticationActivity,MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onStart()
+    {
+        super.onStart()
+        FirebaseAuth.getInstance().addAuthStateListener(this)
+        if(FirebaseAuth.getInstance().currentUser!=null)
+        {
+            startMainActivity()
+        }
+    }
+
+    override fun onStop()
+    {
+        super.onStop()
+        FirebaseAuth.getInstance().removeAuthStateListener(this)
     }
 }
