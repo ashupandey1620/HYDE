4b6f183d2837a7147a660fb9229d78412bca567b
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-01-18 13:05:47 +0530

    third

--- app/src/main/java/com/ashu/hyd/MainActivity.kt
+++ app/src/main/java/com/ashu/hyd/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.ashu.hyd
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.CallLog.Calls
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -11,9 +15,11 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth            : FirebaseAuth
     private lateinit var viewPager2      : ViewPager2
     private lateinit var tabLayout       : TabLayout
     private lateinit var toolbar         : Toolbar
@@ -23,12 +29,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        toolbar = findViewById(R.id.toolbarMain)
-        viewPager2 = findViewById(R.id.viewPager2Main)
-        tabLayout = findViewById(R.id.tabLayoutMain)
-        toolbar.title = "HYDE"
+        toolbar       =  findViewById(R.id.toolbarMain)
+        auth          =  FirebaseAuth.getInstance()
+        viewPager2    =  findViewById(R.id.viewPager2Main)
+        tabLayout     =  findViewById(R.id.tabLayoutMain)
+        toolbar.title =  "HYDE"
         setSupportActionBar(toolbar)
-        appPagerAdapter = AppPagerAdapter(this)
+        appPagerAdapter    = AppPagerAdapter(this)
         viewPager2.adapter = appPagerAdapter
         TabLayoutMediator(tabLayout,viewPager2){
             tab,position->
@@ -56,6 +63,38 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.profile ->{
+                val intent = Intent(this,MenuActivity::class.java)
+                intent.putExtra("option","profile")
+                startActivity(intent)
+
+            }
+            R.id.LogOut -> {
+                auth.signOut()
+                val intent = Intent(this,AuthenticationActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            R.id.AboutUs ->{
+                val intent = Intent(this,MenuActivity::class.java)
+                intent.putExtra("option","about")
+                startActivity(intent)
+
+        }
+
+
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
