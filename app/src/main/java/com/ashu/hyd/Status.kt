39d7da9c7f5f50e13cf347d9cef7ee96bcb4b026
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-06-26 23:38:35 +0530

    Splash ACtivity Time CHAnges

--- app/src/main/java/com/ashu/hyd/Status.kt
+++ app/src/main/java/com/ashu/hyd/Status.kt
@@ -11,23 +11,9 @@ import android.view.ViewGroup
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Status.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Status : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class Status : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater , container: ViewGroup? ,
