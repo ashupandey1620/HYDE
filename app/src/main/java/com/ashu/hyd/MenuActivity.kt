15725d1bd4574e538da6d9fb319a7903ee292f1b
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-06-28 10:11:23 +0530

    Splash ACtivity Time CHAnges

--- app/src/main/java/com/ashu/hyd/MenuActivity.kt
+++ app/src/main/java/com/ashu/hyd/MenuActivity.kt
@@ -2,10 +2,35 @@ package com.ashu.hyd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.FrameLayout
+import android.widget.Toolbar
 
 class MenuActivity : AppCompatActivity() {
+    private lateinit var toolbarMenu: Toolbar
+    private lateinit var frameLayout: FrameLayout
+    private lateinit var optionValue: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        toolbarMenu = findViewById(R.id.toolbarMenu)
+        frameLayout = findViewById(R.id.frameLayout)
+
+        if(intent!=null)
+        {
+            optionValue = intent.getStringExtra("option").toString()
+            when(optionValue)
+            {
+                "profile"->{
+                    supportFragmentManager.beginTransaction().replace(R.id.frameLayout,ProfileFragment()).commit()
+                    toolbarMenu.title = "Profile"
+                }
+                "about"->{
+                    supportFragmentManager.beginTransaction().replace(R.id.frameLayout,AboutFragment()).commit()
+                    toolbarMenu.title = "About"
+                }
+            }
+        }
     }
 }
