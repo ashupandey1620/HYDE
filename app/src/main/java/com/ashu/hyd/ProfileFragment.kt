d774f351271671b1b3a32f8cf2fcf0ddb8450252
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-06-28 15:32:29 +0530

    Splash ACtivity Time CHAnges

--- app/src/main/java/com/ashu/hyd/ProfileFragment.kt
+++ app/src/main/java/com/ashu/hyd/ProfileFragment.kt
@@ -5,8 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var profilePic : CircleImageView
+    private lateinit var profilePicAdd : ImageView
+
+    private lateinit var nameVis : TextView
+    private lateinit var emailVis : TextView
+    private lateinit var statusVis : TextView
+
+    private lateinit var nameEdit : TextInputEditText
+    private lateinit var emailEdit : TextInputEditText
+    private lateinit var statusEdit : TextInputEditText
+
+    private lateinit var updateButton : Button
+    private lateinit var saveButton : Button
+
 
     override fun onCreateView(
         inflater: LayoutInflater , container: ViewGroup? ,
