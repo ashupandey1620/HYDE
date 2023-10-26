05b990c9c025d49d7e0fb900ae5053bd40d58bda
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-10-26 14:46:06 +0530

    Re-Enter Project

---
+++ app/src/main/java/com/ashu/hyd/Adapter/ContactsAdapter.java
@@ -0,0 +1,4 @@
+package com.ashu.hyd.Adapter;
+
+public class ContactsAdapter {
+}
--- app/src/main/java/com/ashu/hyd/ContactFragment.kt
+++ app/src/main/java/com/ashu/hyd/ContactFragment.kt
@@ -1,21 +1,34 @@
 package com.ashu.hyd
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ashu.hyd.Adapter.ContactsAdapter
 
 
 class ContactFragment : Fragment() {
 
+    private lateinit var contactsRecyclerView  :RecyclerView
+    private lateinit var contactsLayoutManager: RecyclerView.LayoutManager
+    private lateinit var contactsAdapter : ContactsAdapter
     override fun onCreateView(
         inflater: LayoutInflater , container: ViewGroup? ,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact , container , false)
+        val view =  inflater.inflate(R.layout.fragment_contact , container , false)
+        contactsRecyclerView = view.findViewById(R.id.contactsRV)
+
+        contactsLayoutManager = LinearLayoutManager(context as Activity)
+
+
+
+        return view
     }
 
 
